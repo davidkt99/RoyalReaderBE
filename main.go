04b20aa1d4492deaed3fa9d9ab352c9aa1a23255
David Thompson <49345822+davidkt99@users.noreply.github.com>
2023-03-16 21:20:32 -0500

    scrapChapter func

--- main.go
+++ main.go
@@ -2,19 +2,45 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
-	resp, err := http.Get("https://www.royalroad.com/fiction/syndication/65359")
-	if err != nil {
-		fmt.Println("Error: ", err)
+	scrapChapter("https://www.royalroad.com/fiction/65359/ashes-of-heaven-book-one/chapter/1138407/chapter-24-the-great-dao-of-scamming")
+
+}
+
+func download(url string) []byte {
+	resp, loadErr := http.Get(url)
+	if loadErr != nil {
+		fmt.Println("Error: ", loadErr)
+		os.Exit(1)
+	}
+	// time.Sleep(1000 * time.Millisecond)
+
+	// io.Copy(os.Stdout, resp.Body)
+
+	dataInBytes, readErr := ioutil.ReadAll(resp.Body)
+	if readErr != nil {
+		fmt.Println("Error: ", readErr)
 		os.Exit(1)
 	}
 
-	bs := make([]byte, 99999)
-	resp.Body.Read(bs)
+	return dataInBytes
+}
 
-	fmt.Println(string(bs))
+func scrapChapter(url string) {
+	page := download(url)
+
+	_, cut, _ := strings.Cut(string(page), "<div class=\"chapter-inner chapter-content\">")
+	chapter, _, _ := strings.Cut(string(cut), "</div>")
+
+	writeErr := ioutil.WriteFile("output.txt", []byte(chapter), 0644)
+	if writeErr != nil {
+		panic(writeErr)
+	}
+	fmt.Println(string(chapter))
 }
