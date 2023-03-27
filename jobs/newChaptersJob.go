09c4dea26963abce3ad76ac9e6df48b6d9fa977d
David Thompson <49345822+davidkt99@users.noreply.github.com>
2023-03-27 18:55:27 -0500

    Added dockerish

--- jobs/newChaptersJob.go
+++ jobs/newChaptersJob.go
@@ -14,7 +14,7 @@ func NewChaptersJob() {
 		storedChapterNum := db.QueryNumOfChapters(book.Id)
 		numOfNewChapters := currChapterNum - storedChapterNum
 		if numOfNewChapters > 0 {
-			fmt.Println("There are new Chapters for ", book.Name, book.Id, book.Url)
+			fmt.Println("There are new Chapters for ", book.Name, book.Id, book.Url, " - Num of New Chapters: ", numOfNewChapters)
 			chapters := scraper.ScrapeNewChapters(book.Url, storedChapterNum)
 			for _, chapter := range chapters {
 				chapter.BookId = book.Id
