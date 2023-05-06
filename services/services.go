0198142841001758737f443acbbaca487d01fc22
David Thompson <49345822+davidkt99@users.noreply.github.com>
2023-05-06 16:30:48 -0500

    Fix XMPHTTP request error in flutter website

--- services/services.go
+++ services/services.go
@@ -1,11 +1,18 @@
 package services
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func StartServices() {
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowAllOrigins: true,
+		AllowMethods:    []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD"},
+		AllowHeaders:    []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
+		ExposeHeaders:   []string{"Content-Length"},
+	}))
 	router.GET("/hello", SayHello)
 	router.GET("/book/chapters/:id", GetBookChapters)
 	router.GET("/book/:id", GetBook)
